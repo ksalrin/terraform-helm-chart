241049a1786791fd5f12ee5fa1d9be3570abf8e3
fuchicorp-scripts <ksalrin@bastion-fuchicorp-com.us-central1-a.c.geometric-sled-282503.internal>
2021-02-11 23:50:22 +0000

    added to version.tf

--- versions.tf
+++ versions.tf
@@ -2,15 +2,23 @@
 # required helm provider for this module 
 provider "helm" { 
     version = "1.0.0"
+     alias = "default"
 }
 
 # Required provider for local files 
 provider "local" {
     version = "1.4.0"
+     alias = "default"
 }
 
 # We are using 2.1.2 for helm deplpoy 
 provider "template" {
     version  = "2.1.2"
+    alias = "default"
+}
+
+provider "google" {
+    version  = "~>1.0"
+    alias = "default"
 }
 
