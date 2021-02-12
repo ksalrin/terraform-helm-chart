3900cb50e023391f60e569040d77081a6eb4e980
Kelly <66441983+ksalrin@users.noreply.github.com>
2021-02-11 18:35:26 -0600

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,24 +1,25 @@
 terraform {
   required_version = ">= 0.11.2"
 }
+
 provider "google" {
-    version  = "~>1.0"
-    alias = "default"
-}
+  version = "~>1.0"
+  alias   = "default"
+  }
 # required helm provider for this module 
 provider "helm" { 
-    version = "0.10.6"
-     alias = "default"
+  version = "0.10.6"
+   alias = "default"
 }
 
 # Required provider for local files 
 provider "local" {
-    version = "1.4.0"
-     alias = "default"
+  version = "1.4.0"
+   alias = "default"
 }
 
 # We are using 2.1.2 for helm deplpoy 
 provider "template" {
-    version  = "2.1.2"
-    alias = "default"
+  version  = "2.1.2"
+  alias = "default"
 }
